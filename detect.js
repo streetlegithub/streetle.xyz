e44032ca4967d4b2b04c226d83f62c26643194ea
Leon <streetlegithub@gmail.com>
2023-11-10 21:34:07 +0000

    testing new adblock

--- detect.js
+++ detect.js
@@ -2,37 +2,45 @@ var img = new Image();
 img.src = 'https://googleads.g.doubleclick.net/pagead/viewthroughconversion/1234567890/?value=0&guid=ON&script=0';
 
 var timeout = setTimeout(function () {
-	displayAdblockPopup();
-	disableHeader();
-	disableFooter();
-}, 3000);
+    displayAdblockPopup();
+    disableHeader();
+    disableFooter();
+}, 6000);
 
 img.onload = function () {
-	clearTimeout(timeout);
+    clearTimeout(timeout);
 };
 
 function displayAdblockPopup() {
-	var mainContent = document.querySelector('main');
-	mainContent.classList.add('mainelement');
+    var downloadButton = document.querySelector('.downloadbutton');
+    
+    if (downloadButton) {
+        downloadButton.style.backgroundColor = 'red';
+        downloadButton.style.cursor = 'not-allowed';
+        downloadButton.innerText = 'AdBlock Detected';
+        downloadButton.onclick = function () {
+            return false; // Prevent click action
+        };
+    }
 
-	var contents = document.createElement('div');
-	contents.className = 'contents';
-	contents.innerHTML = '<h2>Looks like you\'re using AdBlock</h2>' +
-		'<p>That\'s fine, who doesn\'t? But without funding from advertisements, we can\'t keep our content free.</p>' +
-		'<p>Support your smaller creators.</p>';
-	document.body.appendChild(contents);
+    var contents = document.createElement('div');
+    contents.className = 'contents';
+    contents.innerHTML = '<h2>Looks like you\'re using AdBlock</h2>' +
+        '<p>That\'s fine, who doesn\'t? But without funding from advertisements, we can\'t keep our content free.</p>' +
+        '<p>Support your smaller creators.</p>';
+    document.body.appendChild(contents);
 }
 
 function disableHeader() {
-	var header = document.querySelector('.header');
-	if (header) {
-		header.style.pointerEvents = 'none';
-	}
+    var header = document.querySelector('.header');
+    if (header) {
+        header.style.pointerEvents = 'none';
+    }
 }
 
 function disableFooter() {
-	var footer = document.querySelector('footer');
-	if (footer) {
-		footer.style.display = 'none';
-	}
+    var footer = document.querySelector('footer');
+    if (footer) {
+        footer.style.display = 'none';
+    }
 }
