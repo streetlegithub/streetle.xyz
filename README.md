90e93cb310b87c49bd613b37c7618b554f579ce9
Leon <streetlegithub@gmail.com>
2023-11-29 15:05:37 +0000

    Complete Re-design

--- README.md
+++
@@ -1,2 +0,0 @@
-# streetle.xyz
-A website dedicated to my Minecraft Addons/Worlds<br>Previously known as "streetle.ml"
---
+++ scripts/index-dark-mode.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    const isDarkModeStored = localStorage.getItem('darkMode') === 'true';
+    document.body.classList.toggle('dark-mode', isDarkModeStored);
+    document.querySelector('.latest-work').classList.toggle('dark-mode', isDarkModeStored);
+    document.querySelector('.latest-work-header h2').classList.toggle('dark-mode', isDarkModeStored);
+    document.querySelector('.addons-row-advertisement').classList.toggle('dark-mode', isDarkModeStored);
+    document.querySelector('header').classList.toggle('dark-mode', isDarkModeStored);
+
+    const addons = document.querySelectorAll('.addon');
+    addons.forEach(addon => {
+        addon.classList.toggle('dark-mode', isDarkModeStored);
+    });
+
+    darkModeToggle.classList.toggle('dark-mode', isDarkModeStored);
+
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    darkModeToggle.textContent = isDarkMode ? '☀️' : '🌑';
+
+    darkModeToggle.addEventListener('click', toggleDarkMode);
+
+    function toggleDarkMode() {
+        document.body.classList.toggle('dark-mode');
+        document.querySelector('.latest-work').classList.toggle('dark-mode');
+        document.querySelector('.latest-work-header h2').classList.toggle('dark-mode');
+        document.querySelector('.addons-row-advertisement').classList.toggle('dark-mode');
+        document.querySelector('header').classList.toggle('dark-mode');
+
+        const addons = document.querySelectorAll('.addon');
+        addons.forEach(addon => {
+            addon.classList.toggle('dark-mode');
+        });
+
+        darkModeToggle.classList.toggle('dark-mode');
+
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        localStorage.setItem('darkMode', isDarkMode);
+        darkModeToggle.textContent = isDarkMode ? '☀️' : '🌑';
+    }
+});
